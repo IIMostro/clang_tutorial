b64b8f84c00be1409ba58ce9c4e82f90464e7ce0
li.bowei <1306475495@qq.com>
2022-02-07 16:01:43 +0800

    C语言中的变量

--- 04.variable.c
+++ 04.variable.c
@@ -2,7 +2,13 @@
 // Created by 86158 on 2022/1/20.
 //
 #include "stdio.h"
+
+const int glob_pin = 123;
+
 int main(){
-    printf("hello variable");
+    int local_pin = 234;
+    printf("hello variable\n");
+    printf("glob_pin: %d, local_pin: %d \n", glob_pin, local_pin);
+    printf("glob_pin_address: %x\n", &glob_pin);
     return 0;
 }
